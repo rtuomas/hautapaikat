6bd5b941115be24babd0aa53c080d9113a0917b6
Tuomas Rajala <tuomas.a.rajala@gmail.com>
2021-09-30 12:48:23 +0300

    testing

--- back/server.js
+++ back/server.js
@@ -1,1 +1,25 @@
-//TEST
+require('dotenv').config()
+const express = require('express');
+const app = express();
+
+
+const port = process.env.PORT;
+
+app.get('/', (req, res) => {
+
+  res.json({
+      name: 'Gösta Sundqvist',
+      birthday: '17.05.1957',
+      died: '16.08.2003',
+      cemetery: 'Kellonummen hautausmaa',
+      location: {
+          lat: 60.236842,
+          long: 24.642426
+      }
+  })
+
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`)
+});
