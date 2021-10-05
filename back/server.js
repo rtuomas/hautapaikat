ca8733d9b5879d5ea6a1f44ec7c3e5396c03ba4f
Tuomas Rajala <tuomas.a.rajala@gmail.com>
2021-10-05 20:20:30 +0300

    mongoose example added

--- back/server.js
+++ back/server.js
@@ -1,27 +1,76 @@
 require('dotenv').config()
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
 const app = express();
 
 
-const port = process.env.PORT;
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+
+const PORT = process.env.PORT;
+const DB_URL = process.env.DB_URL;
+
+
+mongoose.connect(DB_URL)
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connection to MongoDB:', error.message)
+  })
+
+const deadSchema = new mongoose.Schema({
+  name: String,
+  birthday: Date,
+  died: Date,
+  cemetery: String,
+  location: {
+    lat: Number, long: Number
+  }
+})
+const Dead = mongoose.model('Dead', deadSchema)
+
+
+
 
 app.get('/', (req, res) => {
 
-  res.json({
-      name: 'Gösta Sundqvist',
-      birthday: '17.05.1957',
-      died: '16.08.2003',
-      cemetery: 'Kellonummen hautausmaa',
-      location: {
-          lat: 60.236842,
-          long: 24.642426
-      }
+  Dead.find().then( result => {
+
+    console.log(result)
+    res.json(result)
+
+
   })
+  .catch(error => {
+    console.log(error)
+  });
 
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`)
+app.post('/addDead', (req, res) => {
+
+  console.log(req.body)
+  const person = req.body
+
+
+  const newPerson = new Dead({
+    name: person.name,
+    birthday: person.birthday,
+    died: person.died,
+    cemetery: person.cemetery,
+    location: { lat: person.location.lat, long: person.location.long }
+  });
+  newPerson.save()
+
+  res.json( {message: "Toimii ja tallennettu"} )
+
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`)
 });
 
 // kommentti
